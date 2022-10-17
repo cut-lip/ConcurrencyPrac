043d5622d957860da88a285db61c6d9284bd2614
Nicholas Cutlip <cutlipn@gmail.com>
2022-10-16 20:37:34 -0700

    Initial commit

--- src/SimpleThreads.java
+++ src/SimpleThreads.java
@@ -1,4 +1,22 @@
 // Getting used to working with java.util.concurrent
 
 public class SimpleThreads {
+
+    static void threadMessage(String message)
+    {
+
+    }
+
+    private static class MessageLoop
+        implements Runnable {
+        public void run()
+        {
+
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
